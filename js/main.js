a18d59536a7351f949ddc987f7f9110aefe0dfee
shehabull <shehabull@gmail.com>
2022-09-26 16:20:53 +0600

    student counter template

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    saveEl.textContent += count + " - ";
+    count = 0;
+    countEl.textContent = 0;
+}
